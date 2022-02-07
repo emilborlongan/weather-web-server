c889d4cc225c0c1a964a92aeca5977202b4872b6
Emil Borlongan <emilb@magenic.com>
2022-02-07 22:29:33 +0800

    Heroku Changes

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
     para2.textContent = ''
 
     const location = searchTerm.value
-    fetch('http://localhost:3000/weather?address=' + location).then((response) =>{
+    fetch('/weather?address=' + location).then((response) =>{
         response.json().then((data)=>{
             if(data.error){
                para1.textContent = data.error
